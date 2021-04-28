200b933cbeafeeb7df65e64c9c6d00f48fc88d17
Steve Ouma <Steve@Steve.local>
2021-04-28 18:23:09 +0300

    update tests

--- src/index.js
+++ src/index.js
@@ -1,103 +1,109 @@
 import {
-  mainRule,
-  pseudoSelectorRules,
-  atRules,
-  globalRules,
-  generateID,
-  generateIDForTests,
-  insertStyleAndSetclassIDs,
-  isTestEnvironment,
+    mainRule,
+    pseudoSelectorRules,
+    atRules,
+    globalRules,
+    generateID,
+    generateIDForTests,
+    insertStyleAndSetclassIDs,
+    isTestEnvironment
 } from './utils';
 
 import generateClassID from './generateClassID';
 
 var sheet =
-  typeof document === 'undefined'
-    ? { insertRule: function() {} }
-    : document.head.appendChild(document.createElement('style')).sheet;
+    typeof document === 'undefined'
+        ? { insertRule: function () {} }
+        : document.head.appendChild(document.createElement('style')).sheet;
 
 function defaultcb(css) {
-  sheet.insertRule(css, sheet.cssRules ? sheet.cssRules.length : 0);
+    sheet.insertRule(css, sheet.cssRules ? sheet.cssRules.length : 0);
 }
 
 function scoped(h, cb) {
-  cb = cb || defaultcb;
-  function styled(elem) {
-    return function(tags) {
-      var fns = [].slice.call(arguments);
-      fns.shift();
-      var rulesForComponent = {};
-      return function(props, children) {
-        var classIDs = [];
-        children = Array.isArray(children) ? children : props.children;
+    cb = cb || defaultcb;
+    function styled(elem) {
+        return function (tags) {
+            var fns = [].slice.call(arguments);
+            fns.shift();
+            var rulesForComponent = {};
+            return function (props, children) {
+                var classIDs = [];
+                children = Array.isArray(children) ? children : props.children;
+                var styles = '';
+                for (var index = 0; index < tags.length; index++) {
+                    styles += tags[index] + (fns[index] ? fns[index](props) : '');
+                }
+
+                console.log(typeof elem);
+                var classID = generateClassID(styles);
+                rulesForComponent[classID] = [];
+
+                insertStyleAndSetclassIDs(
+                    classID,
+                    mainRule(styles, classID),
+                    rulesForComponent,
+                    classIDs,
+                    cb
+                );
+                var rulesPseudoSelector = pseudoSelectorRules(styles, classID);
+                for (var index = 0; index < rulesPseudoSelector.length; index++) {
+                    insertStyleAndSetclassIDs(
+                        classID,
+                        rulesPseudoSelector[index],
+                        rulesForComponent,
+                        classIDs,
+                        cb
+                    );
+                }
+                var rulesAt = atRules(styles, classID);
+                for (var index = 0; index < rulesAt.length; index++) {
+                    insertStyleAndSetclassIDs(
+                        classID,
+                        rulesAt[index],
+                        rulesForComponent,
+                        classIDs,
+                        cb
+                    );
+                }
+                if (rulesForComponent[classID].length === 0) {
+                    delete rulesForComponent[classID];
+                }
+                var attr = Object.assign({}, props);
+                attr.class = classIDs.join(' ') + ' ' + (props.class || props.className || '');
+                if (h.name === 'createElementWithValidation') {
+                    attr.className = attr.class;
+                    delete attr.class;
+                    return h(elem, attr, children);
+                }
+                return h(elem, attr, children);
+            };
+        };
+    }
+    styled.keyframes = function (tags) {
+        var args = [].slice.call(arguments);
+        args.shift();
         var styles = '';
-        for (var index = 0; index < tags.length; index++) {
-          styles += tags[index] + (fns[index] ? fns[index](props) : '');
+        for (var i = 0; i < tags.length; i++) {
+            styles += tags[i] + (args[i] || '');
         }
-
-        console.log(typeof elem);
-        var classID = generateClassID(styles);
-        rulesForComponent[classID] = [];
-
-        insertStyleAndSetclassIDs(
-          classID,
-          mainRule(styles, classID),
-          rulesForComponent,
-          classIDs,
-          cb
-        );
-        var rulesPseudoSelector = pseudoSelectorRules(styles, classID);
-        for (var index = 0; index < rulesPseudoSelector.length; index++) {
-          insertStyleAndSetclassIDs(
-            classID,
-            rulesPseudoSelector[index],
-            rulesForComponent,
-            classIDs,
-            cb
-          );
-        }
-        var rulesAt = atRules(styles, classID);
-        for (var index = 0; index < rulesAt.length; index++) {
-          insertStyleAndSetclassIDs(classID, rulesAt[index], rulesForComponent, classIDs, cb);
-        }
-        if (rulesForComponent[classID].length === 0) {
-          delete rulesForComponent[classID];
-        }
-        var attr = Object.assign({}, props);
-        attr.class = classIDs.join(' ') + ' ' + (props.class || props.className || '');
-        if (h.name === 'createElementWithValidation') {
-          attr.className = attr.class;
-          delete attr.class;
-          return h(elem, attr, children);
-        }
-        return h(elem, attr, children);
-      };
+        var name = scoped.generateID();
+        cb('@keyframes ' + name + ' { ' + styles + ' }');
+        return name;
     };
-  }
-  styled.keyframes = function(tags) {
-    var args = [].slice.call(arguments);
-    args.shift();
-    var styles = '';
-    for (var i = 0; i < tags.length; i++) {
-      styles += tags[i] + (args[i] || '');
-    }
-    var name = scoped.generateID();
-    cb('@keyframes ' + name + ' { ' + styles + ' }');
-    return name;
-  };
-  styled.global = function(tags) {
-    var args = [].slice.call(arguments);
-    args.shift();
-    var styles = '';
-    for (var i = 0; i < tags.length; i++) {
-      styles += tags[i] + (args[i] || '');
-    }
-    var matches = globalRules(styles);
-    for (var j = 0; j < matches.length; j++) {
-      cb(matches[j]);
-    }
-  };
-  return styled;
+    styled.global = function (tags) {
+        var args = [].slice.call(arguments);
+        args.shift();
+        var styles = '';
+        for (var i = 0; i < tags.length; i++) {
+            styles += tags[i] + (args[i] || '');
+        }
+        var matches = globalRules(styles);
+        for (var j = 0; j < matches.length; j++) {
+            cb(matches[j]);
+        }
+    };
+    return styled;
 }
 
 scoped.generateID = isTestEnvironment() ? generateIDForTests : generateID;
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -4,64 +4,64 @@ import h from 'vhtml';
 import * as utils from '../utils';
 /** @jsx h */
 
-const renderTestApp = styled => {
-  const terrestrialPlanets = ['Mercury', 'Venus', 'Earth', 'Mars'];
+const renderTestApp = (styled) => {
+    const terrestrialPlanets = ['Mercury', 'Venus', 'Earth', 'Mars'];
 
-  const ListItem = ({ text }) => <li>{text}</li>;
+    const ListItem = ({ text }) => <li>{text}</li>;
 
-  const Container = styled('div')`
-    padding: 10px;
-  `;
+    const Container = styled('div')`
+        padding: 10px;
+    `;
 
-  const CenteredListElement = styled(ListItem)`
-    text-align: center;
-  `;
+    const CenteredListElement = styled(ListItem)`
+        text-align: center;
+    `;
 
-  const App = ({ data }) => (
-    <section className="app-root">
-      <h1 id="welcome-heading">Hi!</h1>
+    const App = ({ data }) => (
+        <section className='app-root'>
+            <h1 id='welcome-heading'>Hi!</h1>
 
-      <p>Here is a list of {data.length} planets:</p>
-      <ul>
-        {data.map(text => (
-          <Container>
-            <CenteredListElement text={text} />
-          </Container>
-        ))}
-      </ul>
-    </section>
-  );
+            <p>Here is a list of {data.length} planets:</p>
+            <ul>
+                {data.map((text) => (
+                    <Container>
+                        <CenteredListElement text={text} />
+                    </Container>
+                ))}
+            </ul>
+        </section>
+    );
 
-  return <App data={terrestrialPlanets} />;
+    return <App data={terrestrialPlanets} />;
 };
 
-test('scoped.generateID is public and callable', t => {
-  const scoped = require('../index').default;
+test('scoped.generateID is public and callable', (t) => {
+    const scoped = require('../index').default;
 
-  t.true(scoped.generateID instanceof Function);
+    t.true(scoped.generateID instanceof Function);
 });
 
 test.failing(
-  'scoped.generateID returns different class names for each render in production and development env',
-  t => {
+    'scoped.generateID returns different class names for each render in production and development env',
+    (t) => {
+        const scoped = require('../index').default;
+        const stub = sinon.stub(scoped, 'generateID').callsFake(utils.generateID);
+
+        const styled = scoped(h);
+        const app = renderTestApp(styled);
+        stub.restore();
+
+        t.snapshot(app);
+    }
+);
+
+test('scoped.generateID returns the same ids when DOM nodes remain untouched in test env', (t) => {
     const scoped = require('../index').default;
-    const stub = sinon.stub(scoped, 'generateID').callsFake(utils.generateID);
+    const stub = sinon.stub(scoped, 'generateID').callsFake(utils.generateIDForTests);
 
     const styled = scoped(h);
     const app = renderTestApp(styled);
     stub.restore();
 
     t.snapshot(app);
-  }
-);
-
-test('scoped.generateID returns the same ids when DOM nodes remain untouched in test env', t => {
-  const scoped = require('../index').default;
-  const stub = sinon.stub(scoped, 'generateID').callsFake(utils.generateIDForTests);
-
-  const styled = scoped(h);
-  const app = renderTestApp(styled);
-  stub.restore();
-
-  t.snapshot(app);
 });
